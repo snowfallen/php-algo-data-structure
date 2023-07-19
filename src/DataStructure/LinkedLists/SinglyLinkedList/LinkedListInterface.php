29e8e8fcb692bd920bbd7b17c1abff8fe752a6ba
snowfallen <bondarenkoov1998@gmail.com>
2023-07-19 21:42:13 +0200

    add interface for linked list

---
+++ src/DataStructure/LinkedLists/SinglyLinkedList/LinkedListInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace PhpAlgoDataStructure\DataStructure\LinkedLists\SinglyLinkedList;
+
+require_once 'NodeInterface.php';
+
+interface LinkedListInterface
+{
+    /**
+     * @param string $value
+     * @return void
+     */
+    public function insertAtTheBeginning(string $value): void;
+
+    /**
+     * @return void
+     */
+    public function deleteAtTheBeginning(): void;
+
+    /**
+     * @param string $value
+     * @return void
+     */
+    public function insertAtTheEnd(string $value): void;
+
+    /**
+     * @return void
+     */
+    public function deleteAtTheEnd(): void;
+
+    /**
+     * @return void
+     */
+    public function insertAfter(): void;
+
+    /**
+     * @return void
+     */
+    public function deleteAfter(): void;
+
+    /**
+     * @param string $value
+     * @return void
+     */
+    public function deleteNodeByValue(string $value): void;
+
+    /**
+     * @return NodeInterface
+     */
+    public function getFirstNode(): NodeInterface;
+
+    /**
+     * @return NodeInterface
+     */
+    public function getLastNode(): NodeInterface;
+
+    /**
+     * @return void
+     */
+    public function displayElements(): void;
+
+}
